b186ee99b0aa7d5f70dfd3063e1dcd7cf919867c
KSegovia <segovia.k@icloud.com>
2023-07-20 18:32:26 +0200

    test make:crud

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,17 +2,79 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/espace-pro/messages', name: 'app_message_index', methods: ['GET'])]
+    public function index(MessageRepository $messageRepository): Response
     {
         return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+            'messages' => $messageRepository->findAll(),
         ]);
+    }
+
+    #[Route('/contact', name: 'app_message_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('message/new.html.twig', [
+            'message' => $message,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/espace-pro/messages/{id}', name: 'app_message_show', methods: ['GET'])]
+    public function show(Message $message): Response
+    {
+        return $this->render('message/show.html.twig', [
+            'message' => $message,
+        ]);
+    }
+
+    #[Route('/espace-pro/messages/{id}/edit', name: 'app_message_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Message $message, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('message/edit.html.twig', [
+            'message' => $message,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/espace-pro/messages/{id}', name: 'app_message_delete', methods: ['POST'])]
+    public function delete(Request $request, Message $message, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$message->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($message);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
     }
 }
