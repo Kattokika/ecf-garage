b87180075c5f13649e2dcaacdac9c6e2ab14ebaf
KSegovia <segovia.k@icloud.com>
2023-07-20 18:32:27 +0200

    implementation html

---
+++ assets/garage.js
@@ -0,0 +1,20 @@
+import './styles/garage.css';
+
+import { Carousel } from 'bootstrap'
+
+const menu = document.querySelector(".menu")
+const navLinks = document.querySelector(".nav-links")
+
+menu.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu')
+})
+
+const myCarouselElement = document.querySelector('#carouselGarage')
+if (myCarouselElement) {
+    new Carousel(myCarouselElement, {
+        ride: "carousel",
+        interval: 10000,
+        touch: false
+    })
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('uploader', './assets/upload.js')
+    .addEntry('garage', './assets/garage.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
